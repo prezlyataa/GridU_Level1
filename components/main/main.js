ba975dde9f23559879ef582acb4b5b28880860e4
Yuriy <prezlyata.yuriy@gmail.com>
2018-03-20 12:49:35 +0200

    styles filter popup on main page

--- components/main/main.js
+++ components/main/main.js
@@ -22,3 +22,11 @@ $( function() {
     $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
         " - $" + $( "#slider-range" ).slider( "values", 1 ) );
 } );
+
+
+function selectOnlyThis(id) {
+    for (let i = 1;i <= 3; i++) {
+        document.getElementById(i).checked = false;
+    }
+    document.getElementById(id).checked = true;
+}
